1d1a2d6f782b6b11a0bf37122f5e8a0d31ca9f84
NirCohen1 <81625984+NirCohen1@users.noreply.github.com>
2021-04-14 22:04:02 +0300

    Add files via upload

---
+++ out/production/ATP-Project-PartA/search/BreadthFirstSearch.java
@@ -0,0 +1,51 @@
+package algorithms.search;
+
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Set;
+
+public class BreadthFirstSearch extends ASearchingAlgorithm {
+    public BreadthFirstSearch() {
+        super();
+    }
+    /**
+     * solving the problem by breadth first search algorithm
+     * @param problem: ISearchable problem to solve
+     * @return a solution
+     */
+
+    public Solution solve(ISearchable problem) {
+        if(problem==null)
+            return null;
+        Set<AState> visited=new HashSet<>();
+        AState start=problem.initial();
+        Queue<AState> queue=new LinkedList<>();
+
+        queue.add(start);
+        visited.add(start);
+        while (!queue.isEmpty()) {
+            numOfEvaluated++;
+            AState current = queue.poll();
+            for (AState state : problem.getAllPossibleStates(current)) {
+                if (!visited.contains(state)) {
+                    if(problem.TestGoal(state))
+                        return buildSolution(state);
+                    visited.add(state);
+                    queue.add(state);
+                }
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public String getName() {
+        return "BreadthFirstSearch";
+    }
+
+    @Override
+    public int getNumberOfNodesEvaluated() {
+        return numOfEvaluated;
+    }
+}
