6b192799e1d0dbf26b295e9d6eea7c6a133a3c56
NirCohen1 <81625984+NirCohen1@users.noreply.github.com>
2021-04-18 15:47:02 +0300

    Add files via upload
    more javadoc

--- src/algorithms/search/Solution.java
+++ src/algorithms/search/Solution.java
@@ -2,6 +2,11 @@ package algorithms.search;
 
 import java.util.ArrayList;
 
+/**
+ *  common class for solving problems.
+ *  used for calculating path which presenting the solution.
+ */
+
 public class Solution {
     private ArrayList<AState> solutionPath;
 
@@ -12,5 +17,6 @@ public class Solution {
     public ArrayList<AState> getSolutionPath() {
         return solutionPath;
     }
+
     public void AddToPath(AState state){ solutionPath.add(0,state); }
 }
